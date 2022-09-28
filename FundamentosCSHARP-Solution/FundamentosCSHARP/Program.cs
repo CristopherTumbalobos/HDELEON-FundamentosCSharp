5dd4743e58ae82a8df47a1082596aac96be39142
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-09-28 09:43:19 -0500

    Arreglos y listas

--- FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using FundamentosCSHARP.Models;
 using System;
+using System.Collections.Generic;
 
 namespace FundamentosCSHARP
 {
@@ -7,13 +8,57 @@ namespace FundamentosCSHARP
     {
         static void Main(string[] args)
         {
-            Bebida bebida = new Bebida("Coca Cola", 1000);
-            bebida.Beberse(500);
+            // ARREGLOS
 
-            Console.WriteLine("Queda " + bebida.Cantidad);
+            int[] numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 
-            Cerveza cerveza = new Cerveza(300, "Cristal");
-            cerveza.Beberse(100);
+            //int numero = numeros[0];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Posicion: " + i + " - Elemento: " + numeros[i]);
+            }
+
+            Console.WriteLine("");
+
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("******************************************");
+
+            // COLA (QUEUE): First input First Output (FIFO)
+            // PILA: Last Input First Output (LIFO)
+
+            // LISTAS
+
+            List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            lista.Add(9);
+            lista.Add(10);
+
+            lista.Remove(2);
+
+            foreach (var numero in lista)
+            {
+                Console.WriteLine("Elemento de la lista: " + numero);
+            }
+
+            Console.WriteLine("*****************************************");
+
+            List<Cerveza> cervezas = new List<Cerveza>() { new Cerveza(10, "Cerveza 1") };
+            cervezas.Add(new Cerveza(50, "Cerveza 2"));
+
+            Cerveza erdinger = new Cerveza(500, "Cerveza 3");
+            cervezas.Add(erdinger);
+
+            foreach (var cerveza in cervezas)
+            {
+                Console.WriteLine("Cerveza: " + cerveza.Nombre);
+            }
+
+            Console.ReadLine();
         }
     }
 }
