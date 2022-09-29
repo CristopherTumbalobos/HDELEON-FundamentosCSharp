faa0399469b60d205a434c431fc81a678deaaafa
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-09-28 20:40:44 -0500

    Conexión a base de datos, crear, editar y eliminar contenido

--- FundamentosCSHARP-Solution/FundamentosCSHARP/Models/CervezaBD.cs
+++ FundamentosCSHARP-Solution/FundamentosCSHARP/Models/CervezaBD.cs
@@ -55,5 +55,73 @@ namespace FundamentosCSHARP.Models
 
             return cervezas;
         }
+
+        public void Add(Cerveza cerveza)
+        {
+            //query = "Consulta SQL Server"
+            string query
+                = "insert into cerveza(nombre, marca, alcohol, cantidad) values(@nombre, @marca, @alcohol, @cantidad)";
+            //Realizando la conexión a la Base de Datos (SQL Server)
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                //Agregamos a la Base de Datos los campos de la cerveza insertada
+                command.Parameters.AddWithValue("@nombre", cerveza.Nombre);
+                command.Parameters.AddWithValue("@marca", cerveza.Marca);
+                command.Parameters.AddWithValue("@alcohol", cerveza.Alcohol);
+                command.Parameters.AddWithValue("@cantidad", cerveza.Cantidad);
+
+                connection.Open();
+                //Para ejecutar el "insert" (Codigo del SQL Server)
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        //Id: Es buena practica que este como atributo, pero por didactica se coloca por separado
+        public void Edit(Cerveza cerveza, int Id)
+        {
+            //query = "Consulta SQL Server"
+            string query
+                = "update cerveza set nombre=@nombre, marca=@marca, alcohol=@alcohol, cantidad=@cantidad where id=@id";
+            //Realizando la conexión a la Base de Datos (SQL Server)
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                //Agregamos a la Base de Datos los campos de la cerveza actualizada
+                command.Parameters.AddWithValue("@nombre", cerveza.Nombre);
+                command.Parameters.AddWithValue("@marca", cerveza.Marca);
+                command.Parameters.AddWithValue("@alcohol", cerveza.Alcohol);
+                command.Parameters.AddWithValue("@cantidad", cerveza.Cantidad);
+                command.Parameters.AddWithValue("@id", Id);
+
+                connection.Open();
+                //Para ejecutar el "update" (codigo del SQL Server)
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        public void Delete(int Id)
+        {
+            //query = "Consulta SQL Server"
+            string query
+                = "delete from cerveza where id=@id";
+            //Realizando la conexión a la Base de Datos (SQL Server)
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                //Agregamos a la Base de Datos los campos de la cerveza actualizada
+                command.Parameters.AddWithValue("@id", Id);
+
+                connection.Open();
+                //Para ejecutar el "update" (codigo del SQL Server)
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
     }
 }
--- FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
@@ -9,6 +9,36 @@ namespace FundamentosCSHARP
         static void Main(string[] args)
         {
             CervezaBD cervezaBD = new CervezaBD();
+
+            //Insertamos nueva cerveza
+            //{}: Creamos un microuniverso para que no se repita el objeto "cerveza" con la "cerveza" del foreach
+            //Comentamos para que no inserte más cervezas
+            /*
+            {
+                Cerveza cerveza = new Cerveza(15, "Insertar");
+                cerveza.Marca = "Insertar";
+                cerveza.Alcohol = 6;
+                cervezaBD.Add(cerveza);
+            }
+            */
+
+            //Editamos un registro en la Base de Datos
+            //Comentamos para no editar más cervezas
+            /*
+            {
+                Cerveza cerveza = new Cerveza(5, "Insertar");
+                cerveza.Marca = "Insertar";
+                cerveza.Alcohol = 5;
+                cervezaBD.Edit(cerveza, 5);
+            }
+            */
+
+            //Eliminamos un registro de la Base de Datos
+            {
+                cervezaBD.Delete(5);
+            }
+
+            //Obtener todas las cervezas de la Base de Datos
             var cervezas = cervezaBD.Get();
 
             foreach (var cerveza in cervezas)
