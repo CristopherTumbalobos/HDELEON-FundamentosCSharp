1ffac2a5a2c84f90dc51178047ccf51274661198
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-09-29 23:55:47 -0500

    Envío de información a Servicios Web por HTTP POST, PUT y DELETE

--- FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
@@ -11,24 +11,82 @@ namespace FundamentosCSHARP
 {
     class Program
     {
-        //async: Para realizar otras acciones mientras esperamos la solicitud
+        //async: Para realizar otras acciones mientras esperamos la solicitud asincrona
         static async Task Main(string[] args)
         {
-            //Para obtener el Json de un REST de prueba (jsonplaceholder)
-            string url = "https://jsonplaceholder.typicode.com/posts";
-            HttpClient client = new HttpClient();
+            //////HTTP POST
+            ////Obteniendo el Json de un REST de prueba (jsonplaceholder)
+            //string url = "https://jsonplaceholder.typicode.com/posts";
+            //var client = new HttpClient();
 
-            //await: Esperar a que termina para continuar
-            //Para obtener la respuesta de la solicitud
-            var httpResponse = await client.GetAsync(url);
+            ////Creando el objeto post para enviar
+            //Post post = new Post()
+            //{
+            //    UserId = 50,
+            //    body = "Hola como estan",
+            //    title = "Titulo de Saludo"
+            //};
 
-            //IsSuccessStatusCode: Todo OK (200)
+            ////Serializar el objeto "post" -> Json
+            //var data = JsonSerializer.Serialize<Post>(post);
+            ////Colocar el contenido en una cadena de texto respetando las tildes
+            //HttpContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+            ////Esperando la respuesta a la solicitud POST
+            //var httpResponse = await client.PostAsync(url, content);
+
+            ////200(OK)
+            //if (httpResponse.IsSuccessStatusCode)
+            //{
+            ////Leer el Json como una cadena de texto
+            //    var result = await httpResponse.Content.ReadAsStringAsync();
+
+            ////Deserializar el Json -> objeto
+            //    var postResult = JsonSerializer.Deserialize<Post>(result);
+            //}
+
+            //////HTTP PUT
+            ////Obteniendo el Json de un REST de prueba (jsonplaceholder)
+            //string url = "https://jsonplaceholder.typicode.com/posts/99";
+            //var client = new HttpClient();
+
+            ////Creando el objeto post para editar
+            //Post post = new Post()
+            //{
+            //    UserId = 50,
+            //    body = "Body PUT",
+            //    title = "Title PUT"
+            //};
+
+            ////Serializar el objeto "post" -> Json
+            //var data = JsonSerializer.Serialize<Post>(post);
+            ////Colocar el contenido en una cadena de texto respetando las tildes
+            //HttpContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+            ////Esperando la respuesta a la solicitud PUT
+            //var httpResponse = await client.PutAsync(url, content);
+
+            ////200(OK)
+            //if (httpResponse.IsSuccessStatusCode)
+            //{
+            ////Leer el Json como una cadena de texto
+            //    var result = await httpResponse.Content.ReadAsStringAsync();
+
+            ////Deserializar el Json -> objeto
+            //    var postResult = JsonSerializer.Deserialize<Post>(result);
+            //}
+
+            ////HTTP DELETE
+            //Obteniendo el Json de un REST de prueba (jsonplaceholder)
+            string url = "https://jsonplaceholder.typicode.com/posts/99";
+            var client = new HttpClient();
+
+            //Esperando la respuesta a la solicitud DELETE
+            var httpResponse = await client.DeleteAsync(url);
+
+            //200(OK)
             if (httpResponse.IsSuccessStatusCode)
             {
-                //Para leer la respuesta como un string
-                var content = await httpResponse.Content.ReadAsStringAsync();
-                //Deserializando Json a una lista de "Post"
-                var posts = JsonSerializer.Deserialize<List<Post>>(content);
+                //Leer el Json como una cadena de texto
+                var result = await httpResponse.Content.ReadAsStringAsync();
             }
         }
     }
