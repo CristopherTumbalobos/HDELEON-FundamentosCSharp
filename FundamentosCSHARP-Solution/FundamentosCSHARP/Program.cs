ce71e977fd137d734d7df72884e35b250c4b8806
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-01 12:10:48 -0500

    LINQ

--- FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace FundamentosCSHARP
 {
@@ -14,15 +15,47 @@ namespace FundamentosCSHARP
         //async: Para realizar otras acciones mientras esperamos
         static async Task Main(string[] args)
         {
-            ////Usando la clase genérica con "Cerveza"
-            //var cerveza = new Cerveza() { Alcohol = 5, Cantidad = 500, Marca = "Colima", Nombre = "Ticus" };
-            //Service.SendRequest<Cerveza> service = new Service.SendRequest<Cerveza>();
-            //var CervezaRespuesta = await service.Send(cerveza);
+            List<int> numeros = new List<int>() {64, 1, 4, 5, 6, 7, 2, 3 };
 
-            //Usando la clase genérica con "Post"
-            var post = new Post() { body = "Body prueba", title = "Hola", UserId = 50 };
-            Service.SendRequest<Post> service = new Service.SendRequest<Post>();
-            var PostRespuesta = await service.Send(post);
+            //FUNCIONES DE EXTENSION DE LINQ
+            //Funcion para filtrar
+            var numero7 = numeros.Where(x => x == 7).FirstOrDefault();
+            var numero79 = numeros.Where(x => x == 79).FirstOrDefault();
+            Console.WriteLine(numero7);
+            Console.WriteLine(numero79);
+
+            //Funcion para ordenar de LINQ
+            var numerosOrdenados = numeros.OrderBy(x => x);
+            foreach (var numero in numerosOrdenados)
+            {
+                Console.WriteLine(numero);
+            }
+
+            //Funcion de suma de LINQ
+            var total = numeros.Sum(x => x);
+            Console.WriteLine(total);
+
+            //Funcion de promedio de LINQ
+            var promedio = numeros.Average(x => x);
+            Console.WriteLine(promedio);
+
+            List<Cerveza> cervezas = new List<Cerveza>()
+            {
+                new Cerveza() { Alcohol=7, Cantidad=10, Nombre="Pale Ale", Marca="Minerva"},
+                new Cerveza() { Alcohol=8, Cantidad=5, Nombre="Ticús", Marca="Colima"},
+                new Cerveza() { Alcohol=7, Cantidad=8, Nombre="Stout", Marca="Minerva"},
+                new Cerveza() { Alcohol=6, Cantidad=100, Nombre="Piedra Lisa", Marca="Colima"}
+            };
+
+            // SINTAXIS INTEGRADA DE LINQ: Parecido a SQL Server
+            var cervezasOrdenadas = from x in cervezas
+                                    where x.Nombre == "Pale Ale" && x.Marca == "Minerva"
+                                    orderby x.Nombre
+                                    select x;
+            foreach (var cerveza in cervezasOrdenadas)
+            {
+                Console.WriteLine($"{cerveza.Nombre} {cerveza.Marca}");
+            }
         }
     }
 }
