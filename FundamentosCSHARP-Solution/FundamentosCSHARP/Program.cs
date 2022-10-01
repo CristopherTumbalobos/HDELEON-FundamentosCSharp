eca5a609509a2b346057268f097ae1f4f72d73bd
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-01 12:46:29 -0500

    LINQ para manipular y obtener objetos complejos con subconsultas

--- FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
@@ -15,47 +15,51 @@ namespace FundamentosCSHARP
         //async: Para realizar otras acciones mientras esperamos
         static async Task Main(string[] args)
         {
-            List<int> numeros = new List<int>() {64, 1, 4, 5, 6, 7, 2, 3 };
-
-            //FUNCIONES DE EXTENSION DE LINQ
-            //Funcion para filtrar
-            var numero7 = numeros.Where(x => x == 7).FirstOrDefault();
-            var numero79 = numeros.Where(x => x == 79).FirstOrDefault();
-            Console.WriteLine(numero7);
-            Console.WriteLine(numero79);
-
-            //Funcion para ordenar de LINQ
-            var numerosOrdenados = numeros.OrderBy(x => x);
-            foreach (var numero in numerosOrdenados)
+            //Creando lista de bares con sus propias cervezas
+            List<Bar> bares = new List<Bar>()
             {
-                Console.WriteLine(numero);
-            }
-
-            //Funcion de suma de LINQ
-            var total = numeros.Sum(x => x);
-            Console.WriteLine(total);
-
-            //Funcion de promedio de LINQ
-            var promedio = numeros.Average(x => x);
-            Console.WriteLine(promedio);
-
-            List<Cerveza> cervezas = new List<Cerveza>()
-            {
-                new Cerveza() { Alcohol=7, Cantidad=10, Nombre="Pale Ale", Marca="Minerva"},
-                new Cerveza() { Alcohol=8, Cantidad=5, Nombre="Ticús", Marca="Colima"},
-                new Cerveza() { Alcohol=7, Cantidad=8, Nombre="Stout", Marca="Minerva"},
-                new Cerveza() { Alcohol=6, Cantidad=100, Nombre="Piedra Lisa", Marca="Colima"}
+                new Bar("El bar 1")
+                {
+                    cervezas = new List<Cerveza>
+                    {
+                        new Cerveza() { Alcohol=7, Cantidad=10, Nombre="Pale Ale", Marca="Minerva"},
+                        new Cerveza() { Alcohol=8, Cantidad=5, Nombre="Ticús", Marca="Colima"},
+                        new Cerveza() { Alcohol=7, Cantidad=8, Nombre="Stout", Marca="Minerva"},
+                    }
+                },
+                new Bar("El bar 2")
+                {
+                    cervezas = new List<Cerveza>
+                    {
+                        new Cerveza() { Alcohol=7, Cantidad=8, Nombre="Stout", Marca="Minerva"},
+                        new Cerveza() { Alcohol=6, Cantidad=100, Nombre="Piedra Lisa", Marca="Colima"}
+                    }
+                },
+                new Bar("El bar nuevo")
             };
 
-            // SINTAXIS INTEGRADA DE LINQ: Parecido a SQL Server
-            var cervezasOrdenadas = from x in cervezas
-                                    where x.Nombre == "Pale Ale" && x.Marca == "Minerva"
-                                    orderby x.Nombre
-                                    select x;
-            foreach (var cerveza in cervezasOrdenadas)
-            {
-                Console.WriteLine($"{cerveza.Nombre} {cerveza.Marca}");
-            }
+            //Busca en que bares se encuentran cervezas con nombre "Ticús"
+            //var bar = (from d in bares
+            //          where d.cervezas.Where(c => c.Nombre == "Ticús").Count() > 0
+            //          select d).ToList();
+
+            //Busca en que bares se encuentran cervezas con nombre "Stout"
+            //var bar = (from d in bares
+            //          where d.cervezas.Where(c => c.Nombre == "Stout").Count() > 0
+            //          select d).ToList();
+
+            //LINQ: Puedes crear objetos complejos
+            //LINQ: Puedes realizar consultas sobre consultas
+            //Busca en que bares se encuentran cervezas con nombre "Stout", asimismo nos muestra las bebidas que se encuentran en dichos bares mostrandonos sus atributos
+            var bar = (from d in bares
+                      where d.cervezas.Where(c => c.Nombre == "Stout").Count() > 0
+                      select new BarData(d.Nombre)
+                      {
+                          bebidas = (from b in d.cervezas
+                                        select new Bebida(b.Nombre, b.Cantidad)
+                                        ).ToList()
+                      }
+                      ).ToList();
         }
     }
 }
