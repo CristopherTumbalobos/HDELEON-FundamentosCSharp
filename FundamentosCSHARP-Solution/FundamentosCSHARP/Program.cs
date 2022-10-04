b4ef62e32f769d2f42b718a3e043a25e6a37fa2b
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-03 19:44:44 -0500

    Control de situaciones inesperadas con Excepciones

--- FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP-Solution/FundamentosCSHARP/Program.cs
@@ -7,6 +7,8 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Linq;
+using FundamentosCSHARP.Service;
+using FundamentosCSHARP.Errors;
 
 namespace FundamentosCSHARP
 {
@@ -15,51 +17,38 @@ namespace FundamentosCSHARP
         //async: Para realizar otras acciones mientras esperamos
         static async Task Main(string[] args)
         {
-            //Creando lista de bares con sus propias cervezas
-            List<Bar> bares = new List<Bar>()
+            try
             {
-                new Bar("El bar 1")
-                {
-                    cervezas = new List<Cerveza>
-                    {
-                        new Cerveza() { Alcohol=7, Cantidad=10, Nombre="Pale Ale", Marca="Minerva"},
-                        new Cerveza() { Alcohol=8, Cantidad=5, Nombre="Ticús", Marca="Colima"},
-                        new Cerveza() { Alcohol=7, Cantidad=8, Nombre="Stout", Marca="Minerva"},
-                    }
-                },
-                new Bar("El bar 2")
-                {
-                    cervezas = new List<Cerveza>
-                    {
-                        new Cerveza() { Alcohol=7, Cantidad=8, Nombre="Stout", Marca="Minerva"},
-                        new Cerveza() { Alcohol=6, Cantidad=100, Nombre="Piedra Lisa", Marca="Colima"}
-                    }
-                },
-                new Bar("El bar nuevo")
-            };
+                var searcherBeer = new SearcherBeer();
+                var cantidad = searcherBeer.GetCantidad("Stoasdsaut");
+                Console.WriteLine("TRY: Todo bien");
+            }
+            catch (FieldAccessException ex)
+            {
+                Console.WriteLine("CATCH: Si te he encontrado");           
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("CATCH: Ha caido en una operacion inválida");           
+            }
+            catch(BeerNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //Exception: Excepcion general
+            catch(Exception ex)
+            {
+                Console.WriteLine("CATCH: Una excepcion general");
+            }
+            //Finally: Siempre se ejecuta
+            finally
+            {
+                Console.WriteLine("FINALLY: Esto siempre se ejecuta");
+            }
 
-            //Busca en que bares se encuentran cervezas con nombre "Ticús"
-            //var bar = (from d in bares
-            //          where d.cervezas.Where(c => c.Nombre == "Ticús").Count() > 0
-            //          select d).ToList();
 
-            //Busca en que bares se encuentran cervezas con nombre "Stout"
-            //var bar = (from d in bares
-            //          where d.cervezas.Where(c => c.Nombre == "Stout").Count() > 0
-            //          select d).ToList();
 
-            //LINQ: Puedes crear objetos complejos
-            //LINQ: Puedes realizar consultas sobre consultas
-            //Busca en que bares se encuentran cervezas con nombre "Stout", asimismo nos muestra las bebidas que se encuentran en dichos bares mostrandonos sus atributos
-            var bar = (from d in bares
-                      where d.cervezas.Where(c => c.Nombre == "Stout").Count() > 0
-                      select new BarData(d.Nombre)
-                      {
-                          bebidas = (from b in d.cervezas
-                                        select new Bebida(b.Nombre, b.Cantidad)
-                                        ).ToList()
-                      }
-                      ).ToList();
+
         }
     }
 }
